3290ffbe10821ccabb58f605ce5c31c1900c9f93
JIWON1923 <68676844+JIWON1923@users.noreply.github.com>
2021-04-01 15:58:44 +0900

    Create 6083.py

---
+++ 6083.py
@@ -0,0 +1,8 @@
+a, b, c = map(int, input().split())
+for i in range(a):
+  for j in range(b):
+    for k in range(c):
+      print('{} {} {}'.format(i,j,k))
+print(a*b*c)
+
+#print(f'{i} {j} {k}') -> python 3.6
