959084024f1ebb5896fd14f98d619b5a58f262df
JIWON1923 <68676844+JIWON1923@users.noreply.github.com>
2021-04-12 14:35:29 +0900

    A multiple of three passes, using while loop

--- 6087.py
+++ 6087.py
@@ -1,4 +1,6 @@
-a = int(input())
-for i in range(1,a+1):
-  if i%3 != 0:
-    print(i, end = " ")
+n = int(input())
+i = 0
+while (i < n):
+  i += 1
+  if i % 3 == 0 : continue
+  print(i, end = ' ')
