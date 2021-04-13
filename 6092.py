364eb770e9400156e811ee608728094831699e3c
JIWON1923 <68676844+JIWON1923@users.noreply.github.com>
2021-04-13 20:20:15 +0900

    format(list), make a list[0]*23 using count

--- 6092.py
+++ 6092.py
@@ -1,6 +1,6 @@
 num = int(input())
 a = list(map(int, input().split()))
 student = [0]*23
-for i in a: #a는 반복가능한 형태
-  student[i-1] += 1
-print (" ".join(repr(i) for i in student))
+for i in a:
+  student[i-1] = a.count(i) # count 함수 사용
+print (' '.join(repr(i) for i in student))
