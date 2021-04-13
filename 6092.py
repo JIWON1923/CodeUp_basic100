f2464a094aac7ad85a87eeadde88f2e1d0a9615a
JIWON1923 <68676844+JIWON1923@users.noreply.github.com>
2021-04-13 20:19:50 +0900

    format(list), make a list[0]*23

--- 6092.py
+++ 6092.py
@@ -1,6 +1,6 @@
 num = int(input())
 a = list(map(int, input().split()))
-d = [0]*23
-for i in range(num):
-  d[a[i]-1] += 1
-print(" ".join(repr(i) for i in d))
+student = [0]*23
+for i in a: #a는 반복가능한 형태
+  student[i-1] += 1
+print (" ".join(repr(i) for i in student))
